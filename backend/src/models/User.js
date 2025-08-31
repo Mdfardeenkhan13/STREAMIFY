77a01bb888278d087f1f05d2722021deda75238d
Mdfardeenkhan13 <mdfardeenkhan768@gmail.com>
2025-08-31 19:48:11 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -67,18 +67,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return isPasswordCorrect;
 };
 
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 const User = mongoose.model("User", userSchema);
 
 
